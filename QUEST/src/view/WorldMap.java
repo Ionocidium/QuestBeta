7430f595b96af20b524b6cd0d2572846377ecd20
Kishidle <arcaramon@gmail.com>
2017-11-05 16:05:06 +0800

    worldmap almost done (missing hidden map check)

--- QUEST/src/view/WorldMap.java
+++ QUEST/src/view/WorldMap.java
@@ -5,17 +5,32 @@ import java.awt.EventQueue;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+
+
+
+import model.Exercises;
+import model.User;
+
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.Icon;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.awt.event.ActionEvent;
 import java.awt.Font;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class WorldMap {
 
 	private JFrame frame;
+	private User user;
+	private Exercises question;
 
 	/**
 	 * Launch the application.
@@ -36,13 +51,52 @@ public class WorldMap {
 	/**
 	 * Create the application.
 	 */
-	public WorldMap() {
+	public WorldMap(User user, Exercises question) {
+		this.user = user;
+		this.question = question;
 		initialize();
 	}
 
 	/**
 	 * Initialize the contents of the frame.
 	 */
+	private void changeMap(int areaNum){
+		user.setArea(areaNum);
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/quest", "user", "");
+			stmt = conn.prepareStatement("UPDATE users SET U_Are = ? WHERE U_Num = ?");
+			stmt.setObject(1, areaNum);
+			stmt.setObject(2, user.getUserNumber());
+			stmt.executeUpdate();
+			
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally{
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			try {
+				stmt.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		MainMenu newView = new MainMenu(user, question);
+		newView.setVisible(true);
+		frame.dispose();
+	}
 	private void initialize() {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 645, 585);
@@ -59,29 +113,45 @@ public class WorldMap {
 		label.setBounds(31, 0, 559, 467);
 		panel.add(label);
 		
+		
+		
 		JButton areaBtn_1 = new JButton("");
 		areaBtn_1.setOpaque(false);
 		areaBtn_1.setContentAreaFilled(false);
 		areaBtn_1.setBorderPainted(false);
 		areaBtn_1.addActionListener(new ActionListener(){
+			
 			public void actionPerformed(ActionEvent arg0){
-				JOptionPane.showMessageDialog(null, "testing");
+				changeMap(1);
 			}
 		});
-		areaBtn_1.setBounds(167, 312, 25, 23);
+		areaBtn_1.setBounds(131, 221, 25, 23);
 		panel.add(areaBtn_1);
+
 		
 		JButton areaBtn_2 = new JButton("");
 		areaBtn_2.setOpaque(false);
 		areaBtn_2.setContentAreaFilled(false);
 		areaBtn_2.setBorderPainted(false);
-		areaBtn_2.setBounds(131, 221, 25, 23);
+		areaBtn_2.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent arg0){
+				JOptionPane.showMessageDialog(null, "testing");
+				changeMap(2);
+			}
+		});
+		areaBtn_2.setBounds(167, 312, 25, 23);
 		panel.add(areaBtn_2);
 		
 		JButton areaBtn_3 = new JButton("");
 		areaBtn_3.setOpaque(false);
 		areaBtn_3.setContentAreaFilled(false);
 		areaBtn_3.setBorderPainted(false);
+		areaBtn_3.addActionListener(new ActionListener(){
+			
+			public void actionPerformed(ActionEvent arg0){
+				changeMap(3);
+			}
+		});
 		areaBtn_3.setBounds(222, 202, 25, 23);
 		panel.add(areaBtn_3);
 		
@@ -89,6 +159,12 @@ public class WorldMap {
 		areaBtn_4.setOpaque(false);
 		areaBtn_4.setContentAreaFilled(false);
 		areaBtn_4.setBorderPainted(false);
+		areaBtn_4.addActionListener(new ActionListener(){
+			
+			public void actionPerformed(ActionEvent arg0){
+				changeMap(4);
+			}
+		});
 		areaBtn_4.setBounds(290, 145, 25, 23);
 		panel.add(areaBtn_4);
 		
@@ -96,6 +172,12 @@ public class WorldMap {
 		areaBtn_5.setOpaque(false);
 		areaBtn_5.setContentAreaFilled(false);
 		areaBtn_5.setBorderPainted(false);
+		areaBtn_5.addActionListener(new ActionListener(){
+			
+			public void actionPerformed(ActionEvent arg0){
+				changeMap(5);
+			}
+		});
 		areaBtn_5.setBounds(450, 202, 25, 31);
 		panel.add(areaBtn_5);
 		
@@ -103,6 +185,12 @@ public class WorldMap {
 		areaBtn_6.setOpaque(false);
 		areaBtn_6.setContentAreaFilled(false);
 		areaBtn_6.setBorderPainted(false);
+		areaBtn_6.addActionListener(new ActionListener(){
+			
+			public void actionPerformed(ActionEvent arg0){
+				changeMap(6);
+			}
+		});
 		areaBtn_6.setBounds(360, 300, 25, 31);
 		panel.add(areaBtn_6);
 		
@@ -110,6 +198,12 @@ public class WorldMap {
 		areaBtn_7.setOpaque(false);
 		areaBtn_7.setContentAreaFilled(false);
 		areaBtn_7.setBorderPainted(false);
+		areaBtn_7.addActionListener(new ActionListener(){
+			
+			public void actionPerformed(ActionEvent arg0){
+				changeMap(7);
+			}
+		});
 		areaBtn_7.setBounds(403, 83, 41, 73);
 		areaBtn_7.setEnabled(false); //if area 7 is not unlocked, perform a check to determine if it should be enabled or not(which should be the same for the map)
 		panel.add(areaBtn_7);
@@ -123,4 +217,5 @@ public class WorldMap {
 		returnButton.setBounds(10, 503, 122, 32);
 		frame.getContentPane().add(returnButton);
 	}
+	
 }
