fca789245c69fe4ca85eb90824a96820650735dd
Kishidle <arcaramon@gmail.com>
2017-11-05 14:34:20 +0800

    some more maps

--- QUEST/src/view/WorldMap.java
+++ QUEST/src/view/WorldMap.java
@@ -6,7 +6,12 @@ import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.Icon;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Font;
 
 public class WorldMap {
 
@@ -40,43 +45,82 @@ public class WorldMap {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 800, 585);
+		frame.setBounds(100, 100, 645, 585);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
 		JPanel panel = new JPanel();
-		panel.setBounds(10, 25, 764, 467);
+		panel.setBounds(10, 25, 609, 467);
 		frame.getContentPane().add(panel);
 		panel.setLayout(null);
 		
 		JLabel label = new JLabel("");
 		label.setIcon(new ImageIcon(WorldMap.class.getResource("/img/mainmap-tp.png")));
-		label.setBounds(108, 0, 559, 467);
+		label.setBounds(31, 0, 559, 467);
 		panel.add(label);
 		
-		JLabel nodeLabel_1 = new JLabel("");
-		nodeLabel_1.setBounds(239, 312, 34, 24);
-		panel.add(nodeLabel_1);
+		JButton areaBtn_1 = new JButton("");
+		areaBtn_1.setOpaque(false);
+		areaBtn_1.setContentAreaFilled(false);
+		areaBtn_1.setBorderPainted(false);
+		areaBtn_1.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent arg0){
+				JOptionPane.showMessageDialog(null, "testing");
+			}
+		});
+		areaBtn_1.setBounds(167, 312, 25, 23);
+		panel.add(areaBtn_1);
 		
-		JLabel nodeLabel_2 = new JLabel("");
-		nodeLabel_2.setBounds(205, 221, 34, 24);
-		panel.add(nodeLabel_2);
+		JButton areaBtn_2 = new JButton("");
+		areaBtn_2.setOpaque(false);
+		areaBtn_2.setContentAreaFilled(false);
+		areaBtn_2.setBorderPainted(false);
+		areaBtn_2.setBounds(131, 221, 25, 23);
+		panel.add(areaBtn_2);
 		
-		JLabel label_2 = new JLabel("");
-		label_2.setBounds(294, 202, 34, 24);
-		panel.add(label_2);
+		JButton areaBtn_3 = new JButton("");
+		areaBtn_3.setOpaque(false);
+		areaBtn_3.setContentAreaFilled(false);
+		areaBtn_3.setBorderPainted(false);
+		areaBtn_3.setBounds(222, 202, 25, 23);
+		panel.add(areaBtn_3);
 		
-		JLabel label_3 = new JLabel("");
-		label_3.setBounds(362, 144, 34, 24);
-		panel.add(label_3);
+		JButton areaBtn_4 = new JButton("");
+		areaBtn_4.setOpaque(false);
+		areaBtn_4.setContentAreaFilled(false);
+		areaBtn_4.setBorderPainted(false);
+		areaBtn_4.setBounds(290, 145, 25, 23);
+		panel.add(areaBtn_4);
 		
-		JLabel label_4 = new JLabel("");
-		label_4.setBounds(523, 202, 34, 30);
-		panel.add(label_4);
+		JButton areaBtn_5 = new JButton("");
+		areaBtn_5.setOpaque(false);
+		areaBtn_5.setContentAreaFilled(false);
+		areaBtn_5.setBorderPainted(false);
+		areaBtn_5.setBounds(450, 202, 25, 31);
+		panel.add(areaBtn_5);
 		
-		JLabel label_5 = new JLabel("");
-		label_5.setBounds(431, 301, 34, 30);
-		panel.add(label_5);
+		JButton areaBtn_6 = new JButton("");
+		areaBtn_6.setOpaque(false);
+		areaBtn_6.setContentAreaFilled(false);
+		areaBtn_6.setBorderPainted(false);
+		areaBtn_6.setBounds(360, 300, 25, 31);
+		panel.add(areaBtn_6);
+		
+		JButton areaBtn_7 = new JButton("");
+		areaBtn_7.setOpaque(false);
+		areaBtn_7.setContentAreaFilled(false);
+		areaBtn_7.setBorderPainted(false);
+		areaBtn_7.setBounds(403, 83, 41, 73);
+		areaBtn_7.setEnabled(false); //if area 7 is not unlocked, perform a check to determine if it should be enabled or not(which should be the same for the map)
+		panel.add(areaBtn_7);
+		
+		JButton returnButton = new JButton("Return");
+		returnButton.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		returnButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		returnButton.setBounds(10, 503, 122, 32);
+		frame.getContentPane().add(returnButton);
 	}
-
 }
